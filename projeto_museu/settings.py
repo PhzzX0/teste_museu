70f14f6c3c528377b001ca2a0f6bd31c34ecc3d5
Matheus de Jesus freire da Silva <matheusfreire15rn@gmail.com>
2024-11-09 19:47:57 -0300

    Commit inicial ou atualização do projeto

--- projeto_museu/settings.py
+++ projeto_museu/settings.py
@@ -74,13 +74,24 @@ WSGI_APPLICATION = 'projeto_museu.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
+#Database com mysql
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "banco_museu",
+        "USER": "mysql_banco",
+        "PASSWORD": "12345",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
